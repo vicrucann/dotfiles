67c541764b0e33cee3905e403ec16c4fd7494f3e
Victoria Rudakova <vicrucann@gmail.com>
2015-08-10 16:00:55 -0400

    upd

--- testers/zsh-install.sh
+++ testers/zsh-install.sh
@@ -2,3 +2,17 @@
 
 # tester file to install zsh environment
 
+# before installing Prezto, uninstall oh-my-zsh if any
+uninstall_oh_my_zsh
+
+# clone the repo
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# default config
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+# change default shell to zsh
+sh -s /bin/zsh
