4f75d2e2641cd25b8ec0520382af629dba9e304e
Victoria Rudakova <vicrucann@gmail.com>
2016-01-12 19:52:35 -0500

    cpp template updated for OSG, syntastic cpp config fixed

--- template.cpp
+++ template.cpp
@@ -1,19 +1,15 @@
-#include <cstring>
-#include <vector>
-#include <list>
-#include <algorithm>
 #include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <cassert>
-#include <string>
-#include <queue>
-#include <set>
 
-using namespace std;
+#include <osg/ref_ptr>
+#include <osgViewer/Viewer>
 
-int main(){
-    cerr << "Template" << endl;
-    return 0;
+int main(int, char**){
+
+    osgViewer::Viewer viewer;
+
+    osg::Camera* cam = viewer.getCamera();
+    cam->setClearColor(osg::Vec4(1.0f, 1.0f, 1.0f, 0.f));
+    cam->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    return viewer.run();
 }
--- vimrc
+++ vimrc
@@ -129,7 +129,7 @@ let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
-let g:syntastic_cpp_config_file = ['~/.syntastic_cpp_config_file']
+let g:syntastic_cpp_config_file = '~/.syntastic_cpp_config_file'
 
 " Solarized color scheme
 set t_Co=256
