61d742a8eac66c8dd6002ed34ec55c355a57bc8d
Victoria Rudakova <vicrucann@gmail.com>
2016-08-22 11:14:43 -0400

    template simplified

--- template.cpp
+++ template.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 
-#include <osg/ref_ptr>
-#include <osgViewer/Viewer>
-
-int main(int, char**){
-
-    osgViewer::Viewer viewer;
-
-    osg::Camera* cam = viewer.getCamera();
-    cam->setClearColor(osg::Vec4(1.0f, 1.0f, 1.0f, 0.f));
-    cam->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    return viewer.run();
+int main(int argv, char** argc)
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
 }
